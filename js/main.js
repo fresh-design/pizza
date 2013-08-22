8f4c03de0748f3f08ff6d133cda471a9f9e36660
Oleg Mokhnyuk <olegmokhnyuk@gmail.com>
2013-08-22 18:12:48 +0300

    day2

--- js/main.js
+++ js/main.js
@@ -2,10 +2,25 @@ var screen = $(window).height();
 
 $(document).ready(function() {
 	$(window).scroll(function() {
+
+		$(window).resize(function() {
+		    screen = $(window).height();
+		});
+
 		if ($(this).scrollTop() + 110 > screen) {
 			$("header").addClass("yellow");
 		} else{
 			$("header").removeClass("yellow");
 		}
-	})
+
+		if ($(this).scrollTop() > screen*2) {
+			$("header").addClass("stop");
+		} else{
+			$("header").removeClass("stop");
+		};
+
+		$(".screen1").css("margin-bottom", screen);
+		console.log(screen);
+		
+	});
 });
